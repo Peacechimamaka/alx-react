b7a5c8f67aca529e2819f7bd91e05704a2a4cd44
Peacechimamaka <peaceamaka111@gmail.com>
2024-09-13 00:24:50 +0100

    task

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -5,13 +5,3 @@ function getImmutableObject(object) {
 }
 
 module.exports = getImmutableObject;
-
-const exampleObject = {
-    fear: true,
-    smell: -1033575916.9145899,
-    wall: false,
-    thing: -914767132
-};
-
-console.log(getImmutableObject(exampleObject));
-
